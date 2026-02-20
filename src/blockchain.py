8f31721be3c0fdf90d66e2de0da06281d5907b4d
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-02-20 12:31:11 +0100

    Lesson 32

--- src/blockchain.py
+++ src/blockchain.py
@@ -6,11 +6,11 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction_value=[1]):
     blockchain.append([last_transaction_value, transaction_amount])
 
-tx_amount = float(input('Your transaction amount please: '))
-add_value(tx_amount)
-tx_amount = float(input('Your transaction amount please: '))
-add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=tx_amount)
-tx_amount = float(input('Your transaction amount please: '))
-add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=tx_amount)
+def get_user_input():
+    return float(input('Your transaction amount please: '))
+
+add_value(get_user_input())
+add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=get_user_input())
+add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=get_user_input())
 
 print(blockchain)
