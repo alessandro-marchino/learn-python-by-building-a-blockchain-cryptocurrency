18f4efa47569583f8b6c0d992be5ea5ae6e8a1f1
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-02-20 12:27:21 +0100

    Lesson 30

--- src/blockchain.py
+++ src/blockchain.py
@@ -3,11 +3,11 @@ blockchain = []
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(transaction_amount, last_transaction_value = [1]):
+def add_value(transaction_amount, last_transaction_value=[1]):
     blockchain.append([last_transaction_value, transaction_amount])
 
 add_value(2)
-add_value(0.9, get_last_blockchain_value())
-add_value(10.89, get_last_blockchain_value())
+add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=0.9)
+add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=10.89)
 
 print(blockchain)
