bba9a4b96631ca95fdf4147f42b7dc2fe404fd10
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-02-20 12:52:13 +0100

    Assignment 1

---
+++ assignments/assignment-01.py
@@ -0,0 +1,19 @@
+# 1) Create two variables – one with your name and one with your age
+name = 'Alessandro'
+age = 37
+
+# 2) Create a function which prints your data as one string
+def print_data() -> None:
+    print(f'Name: {name} - Age: {age}')
+
+print_data()
+# 3) Create a function which prints ANY data (two arguments) as one string
+def print_args(arg1: str, arg2: str) -> None:
+    print(f'{arg1} - {arg2}')
+
+print_args(age, name)
+# 4) Create a function which calculates and returns the number of decades you already lived (e.g. 23 = 2 decades)
+def compute_decades() -> int:
+    return age // 10
+
+print(compute_decades())
