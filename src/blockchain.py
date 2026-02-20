b24f4cbd799062f46b214cf803b1f17dbcfc9bda
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-02-20 12:42:40 +0100

    Lesson 35

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,13 +1,26 @@
+# Initializing our blockchain list
 blockchain = []
 
-def get_last_blockchain_value():
+def get_last_blockchain_value() -> list:
+    """ Returns the last value of the current blockchain. """
     return blockchain[-1]
 
-def add_value(transaction_amount, last_transaction_value=[1]):
+
+def add_value(transaction_amount:float, last_transaction_value:list=[1]) -> None:
+    """
+    Append a new value as well as the last transaction value to the blockchain.
+
+    Arguments:
+        :transaction_amount: The amount that should be added
+        :last_transaction_value: The last blockchain transaction (default [1])
+    """
     blockchain.append([last_transaction_value, transaction_amount])
 
-def get_user_input():
+
+def get_user_input() -> float:
+    """ Returns the input of the user (a new transaction amount) as a float. """
     return float(input('Your transaction amount please: '))
+
 
 add_value(get_user_input())
 add_value(last_transaction_value=get_last_blockchain_value(), transaction_amount=get_user_input())
