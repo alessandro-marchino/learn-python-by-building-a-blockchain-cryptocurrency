45aafdfb6b3c217e17ae92d9740fc585d71cc6e6
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-03-06 09:16:38 +0100

    Lesson 97

--- src/blockchain.py
+++ src/blockchain.py
@@ -16,10 +16,10 @@ def get_balance(participant:str)-> float:
     tx_sender = [ [ tx['amount'] for tx in block['transactions'] if tx['sender'] == participant ] for block in blockchain ]
     open_tx_sender = [ tx['amount'] for tx in open_transactions ]
     tx_sender.append(open_tx_sender)
-    amount_sent = functools.reduce(lambda sum, tx: sum + functools.reduce(lambda tx_sum, amount: tx_sum + amount, tx, 0), tx_sender, 0.0)
+    amount_sent = functools.reduce(lambda tx_sum, tx: tx_sum + sum(tx), tx_sender, 0.0)
 
     tx_received = [ [ tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant ] for block in blockchain ]
-    amount_received = functools.reduce(lambda sum, tx: sum + functools.reduce(lambda tx_sum, amount: tx_sum + amount, tx, 0), tx_received, 0.0)
+    amount_received = functools.reduce(lambda tx_sum, tx: tx_sum + sum(tx), tx_received, 0.0)
 
     return amount_received - amount_sent
 
