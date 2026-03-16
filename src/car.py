1c612889e7a155244e4791c7c380c3e63a19aed9
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-03-16 12:59:14 +0100

    Lesson 143

--- src/car.py
+++ src/car.py
@@ -6,12 +6,20 @@ class Car:
     def drive(self) -> None:
         print(f'I am driving but certainly not faster than {self.top_speed}')
 
+    def __repr__(self) -> str:
+        print('Printing...')
+        return f'Top speed: {self.top_speed}, Warnings: {self.warnings}'
+
 car1 = Car()
 car1.drive()
 car1.warnings.append('New warning')
+# print(car1.__dict__)
+print(car1)
 
 car2 = Car(200)
 car2.drive()
+print(car2)
 
 car3 = Car(250)
 car3.drive()
+print(car3)
