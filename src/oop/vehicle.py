48144ca35605419db0b099db705ee3c108940cf5
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-03-16 13:58:53 +0100

    Lesson 145

--- src/oop/vehicle.py
+++ src/oop/vehicle.py
@@ -1,4 +1,4 @@
-class Car:
+class Vehicle:
     def __init__(self, starting_top_speed = 100) -> None:
         self.top_speed = starting_top_speed
         self.__warnings = []
@@ -15,19 +15,3 @@ class Car:
 
     def __repr__(self) -> str:
         return f'Top speed: {self.top_speed}, Warnings: {len(self.__warnings)}'
-
-car1 = Car()
-car1.drive()
-car1.add_warning('New warning')
-car1.add_warning('')
-# print(car1.__dict__)
-print(car1)
-
-car2 = Car(200)
-car2.drive()
-print(car2)
-
-car3 = Car(250)
-car3.drive()
-print(car3)
-print(car3.get_warnings())
