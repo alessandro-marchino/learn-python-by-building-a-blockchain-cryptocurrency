b745dc4fa6526ab8b7c3954d880f5f232a9c28dc
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-03-19 14:03:09 +0100

    Lesson 172

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from oop.node import Node
 
-if __name__ == '__main__':
-    node = Node()
-    node.listen_for_input()
+# if __name__ == '__main__':
+#     node = Node()
+#     node.listen_for_input()
+
+from oop.wallet import Wallet
+wallet = Wallet()
