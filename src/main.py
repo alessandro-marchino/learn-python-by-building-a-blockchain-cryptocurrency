46e38287c698f684bfdf1669dc9be78334135a8a
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2026-03-20 16:00:25 +0100

    Lesson 187

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-from oop.node import Node
+from flask import Flask
+from flask_cors import CORS
+from oop.wallet import Wallet
+
+app = Flask(__name__)
+CORS(app)
+wallet = Wallet()
+
+@app.route('/', methods=[ 'GET' ])
+def get_ui() -> str:
+    return 'This works'
 
 if __name__ == '__main__':
-    node = Node()
-    node.listen_for_input()
+    app.run(port=5000)
+
+
+# from oop.node import Node
+
+# if __name__ == '__main__':
+#     node = Node()
+#     node.listen_for_input()
